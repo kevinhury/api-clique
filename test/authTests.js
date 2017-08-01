44500e186b8da5fc880fc8b2a3f867a675ec4f82
Kevin Hury <kevinh306@gmail.com>
2017-08-01 09:04:49 +0300

    removed old controllers and routes and fixed tests

--- test/authTests.js
+++ test/authTests.js
@@ -1,22 +1,20 @@
 const chai = require('chai')
 const expect = chai.expect
-const assert = chai.assert
-const should = chai.should
-const controller = require('../controllers/auth')
+const accountsService = require('../api/accounts/accounts.service')
 
 describe('Authentication', () => {
   it('generates a valid token', () => {
     const size = 6
-    const token = controller.generateToken(size)
+    const token = accountsService.generateToken(size)
     expect(token.length).to.equal(size, 'Token length should be 6')
     expect(/^\d+$/.test(token)).to.equal(true, 'Token should be numeric')
   })
 
   it('hash password', (done) => {
     const password = 'plaintext'
-    controller.hashPassword(password)
+    accountsService.hashPassword(password)
       .then((hash) => {
-        return controller.comparePassword(password, hash)
+        return accountsService.comparePassword(password, hash)
           .then((equals) => {
             return expect(equals).to.be.true
           })
